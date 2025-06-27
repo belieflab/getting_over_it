c608b670f03c93ec6779dcc17dc4251747496c0b
Jack Monday <jackmonday@Jacks-MacBook-Air.local>
2025-06-27 15:56:26 -0400

    Tested database connection

---
+++ main.R
@@ -0,0 +1,18 @@
+# Main analysis script for this wizaRdry project
+
+# Load wizaRdry library:
+if(!require(wizaRdry)) {install.packages('wizaRdry')}; library(wizaRdry)
+
+# Check available REDCap forms:
+# redcap.index()
+
+# Example NDA request:
+# Create remediation script in nda/
+# nda("cde_dsm5crossad01")
+
+# Your analysis code here
+# What collections are available in the database specified by config.yml if arg = NULL, in this case sing
+mongo.index()
+
+social_prl  <- mongo("social_prl") 
+
